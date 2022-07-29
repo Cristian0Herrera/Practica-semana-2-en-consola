d6e91d85c984c969354405a8a954ddf617d3c234
Cristian Josue Herrera Bermúdez <USSS108822@ugb.edu.sv>
2022-07-29 15:59:30 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_semana_2_en_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
