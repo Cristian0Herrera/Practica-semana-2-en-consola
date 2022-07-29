81ffba2713c96a5e7ccb7e46af9abe1c6bace657
Cristian Josue Herrera Bermúdez <USSS108822@ugb.edu.sv>
2022-07-29 16:01:37 -0600

    Creación de la clase Program

--- Program.cs
+++ Program.cs
@@ -10,7 +10,38 @@ namespace Practica_semana_2_en_consola
     {
         static void Main(string[] args)
         {
+            class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // El valor predeterminado es "not-a-number" que usamos si una operación, como la división, podría provocar un error.
 
+            // Usar una instrucción switch para hacer los cálculos
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    // Pida al usuario que introduzca un divisor distinto de cero
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                // Devolver texto para una entrada de opción incorrecta
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
         }
     }
 }
